382bbb1b0126759b024965e4b8469c08316812da
MAC <mac@MacBook-Pro-MAC.local>
2023-07-13 10:15:19 +0200

    Adding linkedin proxycurl

--- manager/models.py
+++ manager/models.py
@@ -26,3 +26,24 @@ class Skills(models.Model):
     def __str__(self):
         return self.skill
 
+
+class Candidate(models.Model):
+    linkedin = models.CharField(max_length=255)
+    cv = models.CharField(max_length=255)
+    job_position = models.ForeignKey(JobPosition, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ", " + self.job_position.title
+
+
+class Analysis(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    summary = models.CharField(max_length=2000)
+    facts = models.CharField(max_length=2000)
+    analysis_questions = models.CharField(max_length=2000)
+    analysis_answer = models.CharField(max_length=2000)
+    interview_questions = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.candidate.name + ", " + \
+            self.candidate.job_position.title
